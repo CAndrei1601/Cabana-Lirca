3808e83cc09d1be3f88656e3d7c48f30a30d455f
AndreiSGOO7 <andrei@softgenius.com>
2023-03-10 23:26:37 +0200

    finished text & add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nav class="align-center gap-4 p-4" display="none sm:block;">
+      <nuxt-img src="/assets/icons/hamburger-menu.svg" />
+      <NuxtLink to="/default"> Default layout </NuxtLink>
+      <NuxtLink to="/custom"> Custom layout </NuxtLink>
+      <NuxtLink to="/dynamic"> Dynamic layout </NuxtLink>
+      <NuxtLink to="/other"> Other layout </NuxtLink>
+    </nav>
+    <slot />
+  </div>
+</template>
